b327f61699c07ef503193618d8369a92d1e44e81
agbimsonf <agbimson.f@gmail.com>
2019-05-13 17:48:33 -0400

    v

---
+++ app/data/friends.js
@@ -0,0 +1,56 @@
+// People (DATA)
+// =============================================================
+var friends = [
+    {
+        "name":"Margo Robie",
+        "photo":"https://www.biography.com/.image/t_share/MTUxNTgzODk3Mjk2MDUzNTE2/gettyimages-870916736.jpg",
+        "scores":[
+            3,
+            2,
+            5,
+            4,
+            5,
+            3,
+            3,
+            3,
+            3,
+            2
+          ]   
+    },
+    {
+        "name":"Bradley Cooper",
+        "photo":"https://m.media-amazon.com/images/M/MV5BMjM0OTIyMzY1M15BMl5BanBnXkFtZTgwMTg0OTE0NzE@._V1_.jpg",
+        "scores":[
+            4,
+            5,
+            5,
+            1,
+            2,
+            3,
+            4,
+            1,
+            3,
+            1
+          ]   
+    },
+    {
+        "name":"Chris hemsworth",
+        "photo":"https://vignette.wikia.nocookie.net/disney/images/7/73/Chris_Hemsworth.jpg/revision/latest?cb=20180807225753",
+        "scores":[
+            5,
+            1,
+            2,
+            1,
+            5,
+            1,
+            5,
+            3,
+            3,
+            5
+          ]   
+    },
+  ];
+
+  module.exports = friends;
+
+  
